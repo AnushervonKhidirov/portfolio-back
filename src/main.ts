36eb29a5b567cb0ba64f792a74b1a6df499a0a9d
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-21 16:33:19 +0500

    cors params added

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function main() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT ?? 4000;
   await app.listen(port);
 }
