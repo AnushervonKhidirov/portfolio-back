7aed8cb90e2005592c3bcdb3b8f0d53c9ffbd88b
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 19:39:06 +0500

    tasks added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  async findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.tasksService.findOne(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
+import type { TTask } from './tasks.type';
+
 import { Injectable } from '@nestjs/common';
 
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+import { EndpointDB } from '@constant/endpoints';
+
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  async findOne(id: string) {
+    const tasksJson = await this.findAll();
+    return tasksJson.find((achievement) => achievement.id === id);
+  }
+
+  async findAll() {
+    const tasksJson = await readFile(join(process.cwd(), EndpointDB.Tasks), {
+      encoding: 'utf-8',
+    });
+
+    return JSON.parse(tasksJson) as TTask[];
+  }
+}
