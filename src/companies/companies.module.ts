cc0a2683704454e3f3a8a00bf39017f80d650a74
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 19:56:36 +0500

    companies module added

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 
+import { PositionsModule } from 'src/positions/positions.module';
+import { TasksModule } from 'src/tasks/tasks.module';
+import { AchievementsModule } from 'src/achievements/achievements.module';
+
 @Module({
+  imports: [PositionsModule, TasksModule, AchievementsModule],
   providers: [CompaniesService],
   controllers: [CompaniesController],
 })
