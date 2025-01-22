5920ffe3cce1ac8681f59fb2a444fdb0ad838dd3
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 19:56:29 +0500

    companies controller added

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CompaniesService } from './companies.service';
 
 @Controller('companies')
-export class CompaniesController {}
+export class CompaniesController {
+  constructor(private readonly companiesService: CompaniesService) {}
+
+  @Get()
+  async findAll() {
+    return await this.companiesService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.companiesService.findOne(id);
+  }
+}
