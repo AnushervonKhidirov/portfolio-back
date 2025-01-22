c76fa61c48567dfb43debebd4c8de3167e5d764d
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 21:53:04 +0500

    companies service updated

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -8,6 +8,7 @@ import { join } from 'path';
 import { PositionsService } from 'src/positions/positions.service';
 import { TasksService } from 'src/tasks/tasks.service';
 import { AchievementsService } from 'src/achievements/achievements.service';
+import { SkillsService } from 'src/skills/skills.service';
 
 import { EndpointDB } from '@constant/endpoints';
 
@@ -17,6 +18,7 @@ export class CompaniesService {
     private readonly positionsService: PositionsService,
     private readonly tasksService: TasksService,
     private readonly achievementsService: AchievementsService,
+    private readonly skillsService: SkillsService,
   ) {}
 
   async findOne(id: string) {
@@ -30,12 +32,57 @@ export class CompaniesService {
       { encoding: 'utf-8' },
     );
 
-    return JSON.parse(companiesJson) as TCompany[];
+    const companies = JSON.parse(companiesJson) as TCompany[];
+    const companiesResponse = await this.getCompaniesResponse(companies);
+
+    return companiesResponse;
   }
 
-  private async getCompanyResponse(companies: TCompany[]) {
-    const result: TCompanyResponse[] = [];
+  private async getCompanyResponse(company: TCompany) {
+    const position = await this.positionsService.findOne(company.positionId);
+    const allTasks = await this.tasksService.findAll();
+    const allStacks = await this.skillsService.getAvailableSkills();
 
-    return result;
+    const tasks = allTasks.filter((task) => company.tasksIds.includes(task.id));
+
+    const stacks = allStacks.filter((stack) =>
+      company.stackIds.includes(stack.id),
+    );
+
+    const companyResponse = {
+      ...company,
+      position: position.value,
+      tasks,
+      stacks,
+    } as TCompanyResponse;
+
+    if (company.achievementsIds) {
+      const allAchievements = await this.achievementsService.findAll();
+      companyResponse.achievements = allAchievements.filter((achievement) =>
+        company.achievementsIds.includes(achievement.id),
+      );
+    }
+
+    delete companyResponse.positionId;
+    delete companyResponse.tasksIds;
+    delete companyResponse.achievementsIds;
+    delete companyResponse.stackIds;
+
+    return companyResponse;
+  }
+
+  private async getCompaniesResponse(
+    companies: TCompany[],
+    index: number = 0,
+    companiesResponse: TCompanyResponse[] = [],
+  ): Promise<TCompanyResponse[]> {
+    if (companiesResponse.length === companies.length) return companiesResponse;
+    const companyResponse = await this.getCompanyResponse(companies[index]);
+    const newCompaniesResponse = [...companiesResponse, companyResponse];
+    return this.getCompaniesResponse(
+      companies,
+      index + 1,
+      newCompaniesResponse,
+    );
   }
 }
