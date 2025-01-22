8f8abdd15f1b9b5badbe682f50db8f40aece59ea
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-22 21:11:33 +0500

    module imports added

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -5,9 +5,10 @@ import { CompaniesController } from './companies.controller';
 import { PositionsModule } from 'src/positions/positions.module';
 import { TasksModule } from 'src/tasks/tasks.module';
 import { AchievementsModule } from 'src/achievements/achievements.module';
+import { SkillsModule } from 'src/skills/skills.module';
 
 @Module({
-  imports: [PositionsModule, TasksModule, AchievementsModule],
+  imports: [PositionsModule, TasksModule, AchievementsModule, SkillsModule],
   providers: [CompaniesService],
   controllers: [CompaniesController],
 })
