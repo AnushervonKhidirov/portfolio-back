a00b3f82311535af8ee8d8d70a9c73c030de747d
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-30 15:23:41 +0500

    typeorm added to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Skill } from './skills/entity/skill.entity';
+
 import { UserModule } from './user/user.module';
 import { SkillsModule } from './skills/skills.module';
 import { ContactsModule } from './contacts/contacts.module';
@@ -12,6 +17,15 @@ import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Skill],
+    }),
     UserModule,
     SkillsModule,
     ContactsModule,
