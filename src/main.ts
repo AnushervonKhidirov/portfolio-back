17790ada07803c197c6b79cf4680b55917831a54
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-30 19:05:54 +0500

    swagger added to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT ?? 4000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Profile API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(port);
 }
 
