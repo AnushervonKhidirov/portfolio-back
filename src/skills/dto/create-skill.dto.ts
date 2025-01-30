453d0d1f1e1e89ba56d5c8c63680911ae482e86f
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-30 19:21:34 +0500

    add swagger to skills

--- src/skills/dto/create-skill.dto.ts
+++ src/skills/dto/create-skill.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateSkillDto {
+  @ApiProperty({ example: 'NestJS' })
   @IsString()
   @IsNotEmpty()
   name: string;
