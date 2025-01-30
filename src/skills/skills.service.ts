b3c5bbe15e8490d1f98b98f3d426d75f89340220
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-30 18:22:03 +0500

    skill updated

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,114 +1,63 @@
-// TODO: rename methods
-import type {
-  TSkill,
-  TSkillQuery,
-  TAvailableSkill,
-  TSkillResponse,
-  TSkillTypes,
-} from './skills.type';
-
 import { Injectable } from '@nestjs/common';
-import { join } from 'node:path';
-import { readFile } from 'fs/promises';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-import { EndpointDB } from '@constant/endpoints';
+import { Skill } from './entity/skill.entity';
+import { CreateSkillDto } from './dto/create-skill.dto';
 
 @Injectable()
 export class SkillsService {
-  async getSkill(id: string) {
-    const skills = await this.getSkills();
-    return skills.find((skill) => skill.id === id);
-  }
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillRepository: Repository<Skill>,
+  ) {}
 
-  async getSkills(query?: TSkillQuery) {
-    const skillsJson = await readFile(join(process.cwd(), EndpointDB.Skills), {
-      encoding: 'utf-8',
-    });
-
-    let skills = await this.getSkillsResponse(
-      JSON.parse(skillsJson) as TSkill[],
-    );
-
-    if (query?.type) {
-      skills = await this.getSkillsByType(skills, query.type);
+  async findOne(id: string) {
+    try {
+      return await this.skillRepository.findOneBy({ id });
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    if (query?.min_progress) {
-      const minProgress = parseInt(query.min_progress);
-
-      skills = await this.getSkillsByMinProgress(
-        skills,
-        Number.isNaN(minProgress) ? 0 : minProgress,
-      );
+  async findAll() {
+    try {
+      return await this.skillRepository.find();
+    } catch (err) {
+      console.log(err);
     }
-
-    return skills;
   }
 
-  async getAvailableSkills() {
-    const skillsJson = await readFile(
-      join(process.cwd(), EndpointDB.SkillAvailable),
-      {
-        encoding: 'utf-8',
-      },
-    );
+  async create(createSkillDto: CreateSkillDto) {
+    try {
+      const isExist = await this.skillRepository.existsBy({
+        name: createSkillDto.name,
+      });
 
-    return JSON.parse(skillsJson) as TAvailableSkill[];
+      if (isExist) return null;
+
+      const { generatedMaps } =
+        await this.skillRepository.insert(createSkillDto);
+
+      const createdSkill: Skill = {
+        id: generatedMaps[0].id,
+        createdAt: generatedMaps[0].createdAt,
+        name: createSkillDto.name,
+      };
+
+      return createdSkill;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  async getSkillTypes() {
-    const skillsJson = await readFile(
-      join(process.cwd(), EndpointDB.SkillTypes),
-      {
-        encoding: 'utf-8',
-      },
-    );
-
-    return JSON.parse(skillsJson) as TSkillTypes[];
-  }
-
-  private async getSkillsResponse(skills: TSkill[]) {
-    const convertedAvailableSkills = this.convertToObject(
-      await this.getAvailableSkills(),
-    );
-
-    const convertedSkillTypes = this.convertToObject(
-      await this.getSkillTypes(),
-    );
-
-    let skillResponse = [...skills] as TSkillResponse[];
-
-    skillResponse = skillResponse.map((skill) => {
-      skill.name = convertedAvailableSkills[skill.skillId];
-      skill.type = convertedSkillTypes[skill.typeId];
-
-      delete skill.skillId;
-      delete skill.typeId;
-
-      return skill;
-    });
-
-    return skillResponse;
-  }
-
-  private convertToObject(dataList: { id: string; value: string }[]) {
-    const result: { [key: string]: string } = {};
-
-    dataList.forEach((data) => {
-      result[data.id] = data.value;
-    });
-
-    return result;
-  }
-
-  private async getSkillsByType(skills: TSkillResponse[], type: string) {
-    return skills.filter((skill) => skill.type === type);
-  }
-
-  private async getSkillsByMinProgress(
-    skills: TSkillResponse[],
-    progress: number,
-  ) {
-    return skills.filter((skill) => skill.progress >= progress);
+  async delete(id: string) {
+    try {
+      const isExist = await this.skillRepository.existsBy({ id });
+      if (!isExist) return null;
+      return await this.skillRepository.delete(id);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
