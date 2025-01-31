b53245e7b49576a8e8f355bb5af46ac43fe5d577
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 19:51:14 +0500

    grade service updated

--- src/grades/grades.service.ts
+++ src/grades/grades.service.ts
@@ -1,23 +1,69 @@
-import type { TGrade } from './grades.type';
-
 import { Injectable } from '@nestjs/common';
-import { readFile } from 'fs/promises';
-import { join } from 'path';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-import { EndpointDB } from '@constant/endpoints';
+import { GradeEntity } from './entity/grade.entity';
+
+import { CreateGradeDto } from './dto/create-grade.dto';
+import { UpdateGradeDto } from './dto/update-grade.dto';
 
 @Injectable()
 export class GradesService {
+  constructor(
+    @InjectRepository(GradeEntity)
+    private readonly gradeRepository: Repository<GradeEntity>,
+  ) {}
+
   async findOne(id: string) {
-    const grades = await this.findAll();
-    return grades.find((grade) => grade.id === id);
+    try {
+      return await this.gradeRepository.findOneBy({ id });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async findAll() {
-    const gradesJson = await readFile(join(process.cwd(), EndpointDB.Grades), {
-      encoding: 'utf-8',
-    });
+    try {
+      return await this.gradeRepository.find();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    return JSON.parse(gradesJson) as TGrade[];
+  async create(createGradeDto: CreateGradeDto) {
+    try {
+      const isExist = await this.gradeRepository.existsBy({
+        name: createGradeDto.name,
+      });
+
+      if (isExist) {
+        throw new Error(`${createGradeDto.name} grade already exists`);
+      }
+
+      const newGrade = this.gradeRepository.create(createGradeDto);
+      return await this.gradeRepository.save(newGrade);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async update(id: string, updateGradeDto: UpdateGradeDto) {
+    try {
+      const isExist = await this.gradeRepository.existsBy({ id });
+      if (!isExist) throw new Error(`Grade with id: ${id} doesn't exist`);
+      return await this.gradeRepository.update(id, updateGradeDto);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      const isExist = await this.gradeRepository.existsBy({ id });
+      if (!isExist) throw new Error(`Grade with id: ${id} doesn't exist`);
+      return await this.gradeRepository.delete(id);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
