5efe9884363a468bc4d60514bdb615c0441cfcbb
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 19:09:00 +0500

    import position entity in module

--- src/positions/positions.module.ts
+++ src/positions/positions.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PositionsController } from './positions.controller';
 import { PositionsService } from './positions.service';
+import { PositionEntity } from './entity/position.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PositionEntity])],
   controllers: [PositionsController],
   providers: [PositionsService],
-  exports: [PositionsService],
 })
 export class PositionsModule {}
