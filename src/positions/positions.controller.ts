908ccccddd20db133371601c2d92e5fae7810682
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 19:21:09 +0500

    position controller updated

--- src/positions/positions.controller.ts
+++ src/positions/positions.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  ValidationPipe,
+  ConflictException,
+  ParseUUIDPipe,
+  NotFoundException,
+} from '@nestjs/common';
 import { PositionsService } from './positions.service';
+import { CreatePositionDto } from './dto/create-position.dto';
+import { UpdatePositionDto } from './dto/update-position.dto';
 
 @Controller('positions')
 export class PositionsController {
@@ -11,7 +25,38 @@ export class PositionsController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.positionsService.findOne(id);
+  async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    const skill = await this.positionsService.findOne(id);
+    if (!skill) throw new NotFoundException();
+    return skill;
+  }
+
+  @Post()
+  async create(
+    @Body(new ValidationPipe()) createPositionDto: CreatePositionDto,
+  ) {
+    const position = await this.positionsService.create(createPositionDto);
+    if (!position) {
+      throw new ConflictException(
+        `Position '${createPositionDto.name}' already exist`,
+      );
+    }
+
+    return position;
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body(new ValidationPipe()) updatePositionDto: UpdatePositionDto,
+  ) {
+    const position = await this.positionsService.update(id, updatePositionDto);
+    if (!position) throw new NotFoundException();
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    const result = await this.positionsService.delete(id);
+    if (!result) throw new NotFoundException();
   }
 }
