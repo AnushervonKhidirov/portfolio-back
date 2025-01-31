cf5cf2a0eff2b9f2d34ddf00aad56ec9ed7c426b
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 16:03:47 +0500

    acquired skills added

---
+++ src/acquired-skills/dto/update-acquired-skill.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateAcquiredSkillDto } from './create-acquired-skill.dto';
+
+export class UpdateAcquiredSkillDto extends PartialType(CreateAcquiredSkillDto) {}
