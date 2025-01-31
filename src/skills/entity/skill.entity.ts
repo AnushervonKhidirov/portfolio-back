6e46e49f42f9f226f01ee4cace7ba9cc35166e7c
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 18:27:49 +0500

    updated

--- src/skills/entity/skill.entity.ts
+++ src/skills/entity/skill.entity.ts
@@ -1,5 +1,12 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  OneToOne,
+} from 'typeorm';
+
 import { AcquiredSkillEntity } from 'src/acquired-skills/entity/acquired-skills.entity';
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, OneToOne } from 'typeorm';
 
 @Entity({ name: 'skills' })
 export class SkillEntity {
@@ -9,12 +16,9 @@ export class SkillEntity {
   @Column({ unique: true })
   name: string;
 
-  @CreateDateColumn({
-    name: 'created_at',
-    type: 'timestamp',
-  })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
   createdAt: Date;
 
   @OneToOne(() => AcquiredSkillEntity, (acquiredSkill) => acquiredSkill.skill)
-  acquiredSkill: AcquiredSkillEntity
+  acquiredSkill: AcquiredSkillEntity;
 }
