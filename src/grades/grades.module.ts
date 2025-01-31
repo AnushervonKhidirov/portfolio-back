067d5c223fc8630e3d5a726e5195773482c4ebca
Anushervon Khidirov <ankhid98@gmail.com>
2025-01-31 19:42:00 +0500

    grade entity imported in module

--- src/grades/grades.module.ts
+++ src/grades/grades.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GradesController } from './grades.controller';
 import { GradesService } from './grades.service';
+import { GradeEntity } from './entity/grade.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GradeEntity])],
   controllers: [GradesController],
   providers: [GradesService],
-  exports: [GradesService],
 })
 export class GradesModule {}
