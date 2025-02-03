3fa090f14d65f47d5f92650772b6db0f3a2d48bf
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-03 09:43:41 +0500

    task added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { SkillEntity } from './skills/entity/skill.entity';
 import { AcquiredSkillEntity } from './acquired-skills/entity/acquired-skills.entity';
 import { PositionEntity } from './positions/entity/position.entity';
 import { GradeEntity } from './grades/entity/grade.entity';
+import { TaskEntity } from './tasks/entity/task.entity';
 
 import { UserModule } from './user/user.module';
 import { SkillsModule } from './skills/skills.module';
@@ -13,11 +14,10 @@ import { ContactsModule } from './contacts/contacts.module';
 import { SocialLinksModule } from './social-links/social-links.module';
 import { PositionsModule } from './positions/positions.module';
 import { GradesModule } from './grades/grades.module';
-import { TasksModule } from './tasks/tasks.module';
-import { AchievementsModule } from './achievements/achievements.module';
 import { CompaniesModule } from './companies/companies.module';
 import { ProjectsModule } from './projects/projects.module';
 import { LearnedSkillsModule } from './acquired-skills/acquired-skills.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -29,7 +29,13 @@ import { LearnedSkillsModule } from './acquired-skills/acquired-skills.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [SkillEntity, AcquiredSkillEntity, PositionEntity, GradeEntity],
+      entities: [
+        SkillEntity,
+        AcquiredSkillEntity,
+        PositionEntity,
+        GradeEntity,
+        TaskEntity,
+      ],
       synchronize: true,
     }),
     UserModule,
@@ -38,11 +44,10 @@ import { LearnedSkillsModule } from './acquired-skills/acquired-skills.module';
     SocialLinksModule,
     PositionsModule,
     GradesModule,
-    TasksModule,
-    AchievementsModule,
     CompaniesModule,
     ProjectsModule,
     LearnedSkillsModule,
+    TasksModule,
   ],
   controllers: [],
   providers: [],
