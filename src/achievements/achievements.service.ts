60af84f082155f9f19a26f639d4b8a6ef3c9b9b0
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-03 10:14:42 +0500

    achievements added

--- src/achievements/achievements.service.ts
+++ src/achievements/achievements.service.ts
@@ -1,25 +1,71 @@
-import type { TAchievement } from './achievements.type';
-
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-import { readFile } from 'fs/promises';
-import { join } from 'path';
+import { AchievementEntity } from './entity/achievement.entity';
 
-import { EndpointDB } from '@constant/endpoints';
+import { CreateAchievementDto } from './dto/create-achievement.dto';
+import { UpdateAchievementDto } from './dto/update-achievement.dto';
 
 @Injectable()
 export class AchievementsService {
+  constructor(
+    @InjectRepository(AchievementEntity)
+    private readonly achievementRepository: Repository<AchievementEntity>,
+  ) {}
+
   async findOne(id: string) {
-    const achievements = await this.findAll();
-    return achievements.find((achievement) => achievement.id === id);
+    try {
+      return await this.achievementRepository.findOneBy({ id });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async findAll() {
-    const achievementsJson = await readFile(
-      join(process.cwd(), EndpointDB.Achievements),
-      { encoding: 'utf-8' },
-    );
+    try {
+      return await this.achievementRepository.find();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    return JSON.parse(achievementsJson) as TAchievement[];
+  async create(createAchievementDto: CreateAchievementDto) {
+    try {
+      const isExist = await this.achievementRepository.existsBy({
+        value: createAchievementDto.value,
+      });
+
+      if (isExist) throw new Error('Achievement already exist');
+
+      const newAchievement =
+        this.achievementRepository.create(createAchievementDto);
+
+      return await this.achievementRepository.save(newAchievement);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async update(id: string, updateAchievementDto: UpdateAchievementDto) {
+    try {
+      const isExist = await this.achievementRepository.existsBy({ id });
+      if (!isExist) throw new Error(`Achievement with id ${id} doesn't exist`);
+
+      return await this.achievementRepository.update(id, updateAchievementDto);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      const isExist = await this.achievementRepository.existsBy({ id });
+      if (!isExist) throw new Error(`Achievement with id ${id} doesn't exist`);
+
+      return await this.achievementRepository.delete(id);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
--- src/achievements/achievements.type.ts
+++
@@ -1,4 +0,0 @@
-export type TAchievement = {
-  id: string;
-  value: string;
-};
