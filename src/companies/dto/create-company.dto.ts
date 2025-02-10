5946bc5671038cf47f58fbbc97a61d4ce0f710de
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-10 17:08:49 +0500

    companies entity and dto updated

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -19,12 +19,12 @@ export class CreateCompanyDto {
   name: string;
 
   @ApiProperty({ example: 'be2131c3-4807-4838-83f7-9e28fe32252c' })
-  @IsUUID()
+  @IsUUID('4')
   @IsNotEmpty()
   gradeId: string;
 
   @ApiProperty({ example: '58cc01d6-4c52-45b5-87f2-d00dcc13f535' })
-  @IsUUID()
+  @IsUUID('4')
   @IsNotEmpty()
   positionId: string;
 
@@ -49,19 +49,34 @@ export class CreateCompanyDto {
   @ValidateIf((_, value) => value !== undefined)
   endAt: Date;
 
-  @ApiProperty({ example: ['0a4b0215-54e3-4af9-a6bd-a214b9a2ac10', '773a9a17-fe47-4d82-a298-e3e41c5ba595'] })
+  @ApiProperty({
+    example: [
+      '0a4b0215-54e3-4af9-a6bd-a214b9a2ac10',
+      '773a9a17-fe47-4d82-a298-e3e41c5ba595',
+    ],
+  })
   @IsUUID('4', { each: true })
   @IsArray()
   @ArrayNotEmpty()
   taskIds: string[];
 
-  @ApiProperty({ example: ['017252e8-667b-461e-8f8e-6fe005fe8d46', 'ccf11e7a-8980-4a69-8663-b6ccefd39ee5'] })
+  @ApiProperty({
+    example: [
+      '017252e8-667b-461e-8f8e-6fe005fe8d46',
+      'ccf11e7a-8980-4a69-8663-b6ccefd39ee5',
+    ],
+  })
   @IsUUID('4', { each: true })
   @IsArray()
   @ArrayNotEmpty()
   achievementIds: string[];
 
-  @ApiProperty({ example: ['5db2f0c1-7592-4b6d-948f-b1dd54e66e0b', '9b2f4d17-49d0-477f-9eb1-efa395a614ba'] })
+  @ApiProperty({
+    example: [
+      '5db2f0c1-7592-4b6d-948f-b1dd54e66e0b',
+      '9b2f4d17-49d0-477f-9eb1-efa395a614ba',
+    ],
+  })
   @IsUUID('4', { each: true })
   @IsArray()
   @ArrayNotEmpty()
