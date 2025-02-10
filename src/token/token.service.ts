42db8725e0a16d06b1d726194acb8748065e07c7
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-10 11:00:49 +0500

    token epires added to env

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,3 +1,4 @@
+import type { SignOptions } from 'jsonwebtoken';
 import { Injectable } from '@nestjs/common';
 import { sign, verify, decode } from 'jsonwebtoken';
 
@@ -10,16 +11,24 @@ export type TTokenPayload = {
 export class TokenService {
   accessKey: string;
   refreshKey: string;
+  accessExp: SignOptions['expiresIn'];
+  refreshExp: SignOptions['expiresIn'];
 
   constructor() {
     this.accessKey = process.env.TOKEN_ACCESS_KEY;
     this.refreshKey = process.env.TOKEN_REFRESH_KEY;
+    this.accessExp = process.env.TOKEN_ACCESS_EXP as SignOptions['expiresIn'];
+    this.refreshExp = process.env.TOKEN_REFRESH_EXP as SignOptions['expiresIn'];
   }
 
   generate(payload: TTokenPayload) {
     try {
-      const accessToken = sign(payload, this.accessKey, { expiresIn: '1m' });
-      const refreshToken = sign(payload, this.refreshKey, { expiresIn: '2m' });
+      const accessToken = sign(payload, this.accessKey, {
+        expiresIn: this.accessExp,
+      });
+      const refreshToken = sign(payload, this.refreshKey, {
+        expiresIn: this.refreshExp,
+      });
 
       return { accessToken, refreshToken };
     } catch (err) {
