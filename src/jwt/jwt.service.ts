4dd6cd088504b79a527b0c868df7485a336908d5
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-16 14:52:14 +0500

    feat: save token/user pair to db method added

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { JwtEntity } from './entity/jwt.entity';
+import { UserEntity } from 'src/user/entity/user.entity';
+
 import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtService {
   accessSecret: string = process.env.TOKEN_ACCESS_SECRET;
   refreshSecret: string = process.env.TOKEN_REFRESH_SECRET;
+
+  constructor(
+    @InjectRepository(JwtEntity)
+    private readonly tokenRepository: Repository<JwtEntity>,
+  ) {}
 
   generate(payload: string | object | Buffer) {
     try {
@@ -16,6 +26,19 @@ export class JwtService {
       });
 
       return { accessToken, refreshToken };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async save(user: UserEntity, refreshToken: string) {
+    try {
+      const tokenUserPair = this.tokenRepository.create({
+        refreshToken,
+        user,
+      });
+
+      return await this.tokenRepository.insert(tokenUserPair);
     } catch (err) {
       console.log(err);
     }
