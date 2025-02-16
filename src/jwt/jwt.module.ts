b0e58ea69ffa01e88bd072626afd737102c44ff6
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-16 14:51:34 +0500

    jwt and user entities impored

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtService } from './jwt.service';
+import { JwtEntity } from './entity/jwt.entity';
+import { UserEntity } from 'src/user/entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([JwtEntity, UserEntity])],
   providers: [JwtService],
   exports: [JwtService],
 })
