960ef5f4c1c0ac869460547c02f234d9af513719
Anushervon <ankhid98@gmail.com>
2025-02-18 18:44:58 +0500

    Skills api added (#2)
    * feat: skills api added
    
    * feat: service response type added
    
    * refactoring: skills error handling refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+// modules
+import { SkillModule } from './skill/skill.module';
+
+// entities
+import { SkillEntity } from './skill/entity/skill.entity';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -12,9 +18,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [SkillEntity],
       synchronize: true,
     }),
+    SkillModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/skill/skill.service.ts
@@ -0,0 +1,119 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindManyOptions, FindOptionsWhere, Repository } from 'typeorm';
+import { CreateSkillDto } from './dto/create-skill.dto';
+import { UpdateSkillDto } from './dto/update-skill.dto';
+import { SkillEntity } from './entity/skill.entity';
+import { TServiceResponse } from 'src/common/type/service-response.type';
+
+@Injectable()
+export class SkillService {
+  constructor(
+    @InjectRepository(SkillEntity)
+    private readonly skillRepository: Repository<SkillEntity>,
+  ) {}
+
+  async findOne(
+    where: FindOptionsWhere<SkillEntity>,
+  ): TServiceResponse<SkillEntity> {
+    try {
+      const skill = await this.skillRepository.findOneBy(where);
+      if (!skill) return [null, new NotFoundException('Skill not found')];
+      return [skill, null];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async findAll(
+    options?: FindManyOptions<SkillEntity>,
+  ): TServiceResponse<SkillEntity[]> {
+    try {
+      const skills = await this.skillRepository.find(options);
+
+      if (!Array.isArray(skills)) {
+        return [null, new InternalServerErrorException()];
+      }
+
+      return [skills, null];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async create(createSkillDto: CreateSkillDto): TServiceResponse<SkillEntity> {
+    try {
+      const isExist = await this.skillRepository.existsBy({
+        value: createSkillDto.value,
+      });
+
+      if (isExist) {
+        return [
+          null,
+          new ConflictException(`Skill ${createSkillDto.value} already exist`),
+        ];
+      }
+
+      const newSkill = this.skillRepository.create(createSkillDto);
+      const createdSkill = await this.skillRepository.save(newSkill);
+      return [createdSkill, null];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async update(
+    id: number,
+    updateSkillDto: UpdateSkillDto,
+  ): TServiceResponse<SkillEntity> {
+    try {
+      const skill = await this.skillRepository.findOneBy({ id });
+
+      if (!skill) {
+        return [null, new NotFoundException(`Skill with id ${id} not found`)];
+      }
+
+      const existedSkill = await this.skillRepository.findOneBy({
+        value: updateSkillDto.value,
+      });
+
+      if (existedSkill) {
+        return [
+          null,
+          new ConflictException(
+            `Skill '${updateSkillDto.value}' already exist with id ${existedSkill.id}`,
+          ),
+        ];
+      }
+
+      const newSkill = this.skillRepository.create({
+        ...skill,
+        ...updateSkillDto,
+      });
+      const updatedSkill = await this.skillRepository.save(newSkill);
+
+      return [updatedSkill, null];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async delete(id: number): TServiceResponse<SkillEntity> {
+    try {
+      const skill = await this.skillRepository.findOneBy({ id });
+      if (!skill) {
+        return [null, new NotFoundException(`Skill with id ${id} not found`)];
+      }
+
+      await this.skillRepository.delete(id);
+      return [skill, null];
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
