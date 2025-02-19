16bb89b13c175172c1452a7b336fe249c7bd96c9
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-19 08:42:33 +0500

    reafactoring: user service error handling refactored

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindManyOptions, FindOptionsWhere, Repository } from 'typeorm';
 
 import { UserEntity } from './entity/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+
+import { TServiceAsyncMethodReturn } from '@common/type/service-method.type';
 
 @Injectable()
 export class UserService {
@@ -13,52 +20,85 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  async findOne(options: FindOptionsWhere<UserEntity>) {
-    const { password, ...properOption } = options;
-
+  async findOne(
+    options: FindOptionsWhere<UserEntity>,
+  ): TServiceAsyncMethodReturn<UserEntity> {
     try {
+      const { password, ...properOption } = options;
       const user = await this.userRepository.findOneBy(properOption);
-      if (!user) throw new Error('User not found');
-      return user;
+      if (!user) return [null, new NotFoundException('User not found')];
+      return [user, null];
     } catch (err) {
       console.log(err);
     }
   }
 
-  async findAll(options?: FindManyOptions<UserEntity>) {
+  async findAll(
+    options?: FindManyOptions<UserEntity>,
+  ): TServiceAsyncMethodReturn<UserEntity[]> {
     try {
-      return await this.userRepository.find(options);
+      const users = await this.userRepository.find(options);
+
+      if (!Array.isArray(users)) {
+        return [null, new InternalServerErrorException()];
+      }
+
+      return [users, null];
     } catch (err) {
       console.log(err);
     }
   }
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): TServiceAsyncMethodReturn<UserEntity> {
     try {
+      const isExit = await this.userRepository.existsBy({
+        email: createUserDto.email,
+      });
+
+      if (isExit) {
+        return [
+          null,
+          new ConflictException(
+            `User with email '${createUserDto.email}' already exist`,
+          ),
+        ];
+      }
+
       const newUser = this.userRepository.create({
         ...createUserDto,
         createdAt: Date.now(),
       });
-      if (!newUser) throw new Error('Unable to create user');
-      return await this.userRepository.save(newUser);
+      const createUser = await this.userRepository.save(newUser);
+
+      return [createUser, null];
     } catch (err) {
       console.log(err);
     }
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): TServiceAsyncMethodReturn<UserEntity> {
     const user = await this.userRepository.findOneBy({ id });
-    if (!user) throw new Error('User not found');
-    return user;
+    if (!user) return [null, new NotFoundException('User not found')];
+
+    const newUser = this.userRepository.create({
+      ...user,
+      ...updateUserDto,
+    });
+
+    const updatedUser = await this.userRepository.save(newUser);
+    return [updatedUser, null];
   }
 
-  async delete(id: number) {
+  async delete(id: number): TServiceAsyncMethodReturn<UserEntity> {
     try {
       const user = await this.userRepository.findOneBy({ id });
-      if (!user) throw new Error('User not found');
+      if (!user) return [null, new NotFoundException('User not found')];
 
       await this.userRepository.delete(id);
-      return user;
+      return [user, null];
     } catch (err) {
       console.log(err);
     }
