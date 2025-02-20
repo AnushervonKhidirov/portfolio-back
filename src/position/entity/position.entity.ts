f4c41b1f730c7f0f026a4c951b1e1a81999204d9
Anushervon Khidirov <ankhid98@gmail.com>
2025-02-20 09:30:44 +0500

    feat: created_at & updated_at fields added to positions

--- src/position/entity/position.entity.ts
+++ src/position/entity/position.entity.ts
@@ -10,4 +10,16 @@ export class PositionEntity {
   @ApiProperty({ type: 'string', example: 'Back-End' })
   @Column({ length: 20, unique: true })
   value: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'bigint',
+  })
+  createdAt: number;
+
+  @Column({
+    name: 'updated_at',
+    type: 'bigint',
+  })
+  updatedAt: number;
 }
