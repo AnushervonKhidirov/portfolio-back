65bb3716e2fd5b9c8fbc46c218501d69b7ad59a0
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-07 12:04:06 +0500

    feat: activity entity added

---
+++ src/activity/entity/activity.entity.ts
@@ -0,0 +1,73 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { PositionEntity } from 'src/position/entity/position.entity';
+import { ProfileEntity } from 'src/profile/entity/profile.entity';
+
+import { ACTIVITY_TAG } from '../activity.type';
+
+@Entity({ name: 'activities' })
+export class ActivityEntity {
+  @ApiProperty({ example: 1 })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty({ example: ACTIVITY_TAG.EXPERIENCE })
+  @Column({ type: 'enum', enum: ACTIVITY_TAG })
+  tag: ACTIVITY_TAG;
+
+  @ApiProperty({ example: 'Google' })
+  @Column({ length: 30 })
+  name: string;
+
+  @ApiProperty({ example: '2024-01-01' })
+  @Column({ name: 'start_at', type: 'date' })
+  startAt: Date;
+
+  @ApiProperty({ example: '2024-02-18' })
+  @Column({ name: 'end_at', type: 'date' })
+  endAt: Date;
+
+  @ApiProperty({ example: 'About google company' })
+  @Column({ type: 'text' })
+  about: string;
+
+  @ApiProperty({ example: 1740024934772 })
+  @Column({
+    name: 'created_at',
+    type: 'bigint',
+  })
+  createdAt: number;
+
+  @ApiProperty({ example: 1740024934772 })
+  @Column({
+    name: 'updated_at',
+    type: 'bigint',
+  })
+  updatedAt: number;
+
+  @ApiProperty({ example: 1 })
+  @Column({ name: 'position_id' })
+  positionId: number;
+
+  @ManyToOne(() => PositionEntity, (position) => position.activity, {
+    eager: true,
+  })
+  @JoinColumn({ name: 'position_id' })
+  position: PositionEntity;
+
+  @ApiProperty({ example: 1 })
+  @Column({ name: 'profile_id' })
+  profileId: number;
+
+  @ManyToOne(() => ProfileEntity, (profile) => profile.activity, {
+    eager: true,
+  })
+  @JoinColumn({ name: 'profile_id' })
+  profile: ProfileEntity;
+}
