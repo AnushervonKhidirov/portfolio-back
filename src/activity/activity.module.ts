d82865140d7a2f9acad7cba82ea2a306fbfbc9cc
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-07 18:25:32 +0500

    feat: activity module added

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -3,9 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityService } from './activity.service';
 import { ActivityController } from './activity.controller';
 import { ActivityEntity } from './entity/activity.entity';
+import { PositionModule } from 'src/position/position.module';
+import { ProfileModule } from 'src/profile/profile.module';
+import { TaskModule } from 'src/task/task.module';
+import { AchievementModule } from 'src/achievement/achievement.module';
+import { SkillModule } from 'src/skill/skill.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ActivityEntity])],
+  imports: [
+    PositionModule,
+    ProfileModule,
+    TaskModule,
+    AchievementModule,
+    SkillModule,
+    TypeOrmModule.forFeature([ActivityEntity]),
+  ],
   providers: [ActivityService],
   controllers: [ActivityController],
 })
